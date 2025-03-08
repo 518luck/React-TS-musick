752f0190d9f3a63a5f103dab4b98c3d693ef1b4e
蜻蜓ink队长 <1512013298@qq.com>
2025-03-09 00:17:47 +0800

    eslint.config文件配置

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,24 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import pluginReact from 'eslint-plugin-react'
+import prettier from 'eslint-plugin-prettier' // 引入 prettier 插件
+import prettierConfig from 'eslint-config-prettier' // 引入 prettier 配置
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
-];
+  {
+    plugins: {
+      prettier: prettier // 插件应以对象形式出现
+    },
+    rules: {
+      'prettier/prettier': ['error'] // 开启 prettier 检查
+    }
+  },
+  prettierConfig // 直接包含 prettier 配置
+]
