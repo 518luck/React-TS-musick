ada670b7d2510c1458e5fc8609c345c5e95061c0
蜻蜓ink队长 <1512013298@qq.com>
2025-03-09 23:17:56 +0800

    路由懒加载的配置,使用lazy和Suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import { useRoutes, Link } from 'react-router-dom'
 import routes from '@/router'
+import { Suspense } from 'react'
+// 当使用 React.lazy 加载组件时，React 会在加载过程中等待组件的内容。
+// 因此，我们需要将懒加载的组件包裹在 Suspense 组件中，
+// Suspense 负责处理加载过程中的等待和显示加载中的 UI（通过 fallback 属性）。
 
 function App() {
   return (
@@ -10,7 +14,9 @@ function App() {
         <Link to="/focus">关注</Link>
         <Link to="/download">下载客户端</Link>
       </div>
-      <div className="main">{useRoutes(routes)}</div>
+      <Suspense fallback="loading...">
+        <div className="main">{useRoutes(routes)}</div>
+      </Suspense>
     </div>
   )
 }
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,9 +1,28 @@
+import { lazy } from 'react'
+// React.lazy 是 React 内置的一个函数，用于延迟加载一个组件，直到它真正被渲染时才进行加载。
+// 这通常与 动态 import() 语法一起使用。
 import { Navigate } from 'react-router-dom'
 import type { RouteObject } from 'react-router-dom'
+
+/* 
+普通路由,没有懒加载的路由
 import Discover from '../views/discover/index'
 import Mine from '@/views/mine'
 import Focus from '@/views/focus'
-import Download from '@/views/download'
+import Download from '@/views/download' 
+*/
+
+// 进行性能优化,分包,在VUE/React中称之为路由懒加载,使用lazy
+const Discover = lazy(() => import('@/views/discover/index'))
+// import('@/views/discover/index')：这是一种动态 import 语法，返回一个
+// Promise，并指示浏览器按需加载指定的模块（即 @/views/discover/index）
+const Mine = lazy(() => import('@/views/mine/index'))
+const Focus = lazy(() => import('@/views/focus/index'))
+const Download = lazy(() => import('@/views/download/index'))
+
+// import Mine from '@/views/mine'
+// import Focus from '@/views/focus'
+// import Download from '@/views/download'
 
 const routes: RouteObject[] = [
   {
