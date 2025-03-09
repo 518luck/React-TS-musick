fffdd28ee26bb831e81f84520c5b4ca3ac8ba3f8
蜻蜓ink队长 <1512013298@qq.com>
2025-03-09 22:48:21 +0800

    使用Navigate进行路由组件重定向

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
-import React from 'react'
 import { useRoutes } from 'react-router-dom'
 import routes from '@/router'
-import Download from './views/download'
 
 function App() {
   return (
     <div className="App">
-      <Download name="why" age={18}>
-        <div>111</div>
-      </Download>
       <div className="main">{useRoutes(routes)}</div>
     </div>
   )
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,10 +1,30 @@
-import { RouteObject } from 'react-router-dom'
+import { Navigate } from 'react-router-dom'
+import type { RouteObject } from 'react-router-dom'
 import Discover from '../views/discover/index'
+import Mine from '@/views/mine'
+import Focus from '@/views/focus'
+import Download from '@/views/download'
 
 const routes: RouteObject[] = [
   {
+    path: '/',
+    element: <Navigate to="/discover" />
+  },
+  {
     path: '/discover',
     element: <Discover />
+  },
+  {
+    path: '/mine',
+    element: <Mine />
+  },
+  {
+    path: '/focus',
+    element: <Focus />
+  },
+  {
+    path: '/download',
+    element: <Download />
   }
 ]
 
