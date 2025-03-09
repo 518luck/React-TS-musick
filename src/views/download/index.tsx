5141b871ba6a3c7904c19d6674efeaf5436a78d8
蜻蜓ink队长 <1512013298@qq.com>
2025-03-09 21:59:38 +0800

    组件和props的类型约束

--- src/views/download/index.tsx
+++ src/views/download/index.tsx
@@ -1,9 +1,17 @@
+import { memo } from 'react'
+// memo 允许你的组件在 props 没有改变的情况下跳过重新渲染。
+import type { ReactNode } from 'react'
+// 在 TypeScript 中，import type 用于仅导入类型，不会在编译后的 JavaScript 代码中引入对应的模块。
+
 interface Iprops {
+  children?: ReactNode
   name: string
   age: number
   height?: number
 }
-const Download = (props: Iprops) => {
+
+//直接对props进行类型约束
+/* const Download = (props: Iprops) => {
   return (
     <div>
       <div>name:{props.name}</div>
@@ -11,6 +19,17 @@ const Download = (props: Iprops) => {
       <div>height:{props.height}</div>
     </div>
   )
+} */
+
+const Download: React.FC<Iprops> = (props) => {
+  return (
+    <div>
+      <div>name:{props.name}</div>
+      <div>age:{props.age}</div>
+      <div>height:{props.height}</div>
+      <div>{props.children}</div>
+    </div>
+  )
 }
 
-export default Download
+export default memo(Download)
