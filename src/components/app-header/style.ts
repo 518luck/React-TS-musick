3a8d76cf80ab346c469480f00f198e14637a5209
蜻蜓ink队长 <1512013298@qq.com>
2025-03-11 15:55:04 +0800

    安装styled-components

---
+++ src/components/app-header/style.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+// 有时候导入一个模块会因为没有类型生命而报错
+// 三种类型声明
+// 1.typescript内置的,不需要类型声明
+// 2.第三方*库内部已经有类型声明(比如axios),还有react/react-dom=>@type/react(第三方的类型声明)
+// 3.自己写类型声明
