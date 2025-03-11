a2275aa1acef64c22812b8b8459c81bb721a9819
蜻蜓ink队长 <1512013298@qq.com>
2025-03-11 15:20:32 +0800

    类组件实例

---
+++ src/views/demo常见组件定义/类组件.tsx
@@ -0,0 +1,18 @@
+import React, { PureComponent } from 'react'
+
+/**
+ * state
+ * props
+ */
+class Demo02 extends PureComponent {
+  constructor(props: any) {
+    super(props)
+  }
+  render() {
+    return (
+      <div>
+        <div>Demo02</div>
+      </div>
+    )
+  }
+}
