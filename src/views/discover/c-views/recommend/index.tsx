0188f099c6e4fb485f1e72b83aa42bacf22b2752
蜻蜓ink队长 <1512013298@qq.com>
2025-03-11 21:42:35 +0800

    清理代码

--- src/views/discover/c-views/recommend/index.tsx
+++ src/views/discover/c-views/recommend/index.tsx
@@ -1,48 +1,12 @@
-import { memo, useEffect, useState } from 'react'
+import { memo } from 'react'
 import type { FC, ReactNode } from 'react'
-import hyRequest from '@/service'
-import { log } from 'node:console'
 
 interface Iprops {
   children?: ReactNode
 }
 
-interface IBannerDate {
-  imageUrl: string
-  targetId: number
-  adid: any
-  targetType: number
-  titleColor: string
-  typeTitle: string
-  url: string
-  exclusive: boolean
-  scm: string
-  bannerBizType: string
-}
-// 新增响应类型定义
-interface IBannerResponse {
-  banners: IBannerDate[]
-  code: number
-}
 const Recommend: FC<Iprops> = (props) => {
-  //测试网络请求
-  const [banners, setBanners] = useState<IBannerDate[]>([])
-  useEffect(() => {
-    hyRequest
-      .get<IBannerResponse>({
-        url: '/banner'
-      })
-      .then((res) => {
-        setBanners(res.banners)
-      })
-  }, [])
-  return (
-    <div>
-      {banners.map((item, index) => {
-        return <div key={index}>{item.imageUrl}</div>
-      })}
-    </div>
-  )
+  return <div>Recommend</div>
 }
 
 export default memo(Recommend)
