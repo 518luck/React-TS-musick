122b1d0c3bb35f1866b01372e5f3329678f90153
蜻蜓ink队长 <1512013298@qq.com>
2025-03-11 19:03:20 +0800

    Header左侧列表

--- src/components/app-header/index.tsx
+++ src/components/app-header/index.tsx
@@ -1,18 +1,45 @@
 import { memo } from 'react'
 import type { FC, ReactNode } from 'react'
 import { Link } from 'react-router-dom'
+import { HeaderWrapper, HeaderLeft, HeaderRight } from './style'
+
+import headerTitles from '@/assets/data/header-titles.json'
 
 interface IProps {
   children?: ReactNode
 }
 const AppHeader: FC<IProps> = () => {
+  function showItem(item: any) {
+    if (item.type === 'path') {
+      return <Link to={item.link}>{item.title}</Link>
+    } else {
+      return (
+        <a href={item.link} rel="noreferrer" target="_blank">
+          {item.title}
+        </a>
+      )
+    }
+  }
   return (
-    <div className="nav">
-      <Link to="/discover">发现音乐</Link>
-      <Link to="/mine">我的音乐</Link>
-      <Link to="/focus">关注</Link>
-      <Link to="/download">下载客户端</Link>
-    </div>
+    <HeaderWrapper>
+      <div className="conter wrap-v1">
+        <HeaderLeft>
+          <a className="logo sprite_01" href="/#">
+            网易云音乐
+          </a>
+          <div className="titles-list">
+            {headerTitles.map((item) => {
+              return (
+                <div className="item" key={item.title}>
+                  {showItem(item)}
+                </div>
+              )
+            })}
+          </div>
+        </HeaderLeft>
+        <HeaderRight>Right</HeaderRight>
+      </div>
+    </HeaderWrapper>
   )
 }
 export default memo(AppHeader)
