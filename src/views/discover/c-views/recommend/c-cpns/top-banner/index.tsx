5df766450d1f3a41804e223228d816e0d4587e1a
蜻蜓ink队长 <1512013298@qq.com>
2025-03-12 10:22:57 +0800

    控制轮播图跳转

--- src/views/discover/c-views/recommend/c-cpns/top-banner/index.tsx
+++ src/views/discover/c-views/recommend/c-cpns/top-banner/index.tsx
@@ -1,26 +1,60 @@
-import { useAppSelector } from '@/store'
-import { memo } from 'react'
+import { memo, useRef } from 'react'
 import type { FC, ReactNode } from 'react'
-import { shallowEqual } from 'react-redux'
+import { Carousel } from 'antd'
+import type { CarouselRef } from 'antd/es/carousel'
+
+import { useAppSelector, shallowEqualApp } from '@/store'
+import { BannerControl, BannerLeft, BannerRight, BannerWrapper } from './style'
 
 interface Iprops {
   children?: ReactNode
 }
 
-const TopBanner: FC<Iprops> = (props) => {
+const TopBanner: FC<Iprops> = () => {
+  const bannerRef = useRef<CarouselRef>(null)
   /* 从store中获取数据 */
   const { banners } = useAppSelector(
     (state) => ({
       banners: state.recommend.banners
     }),
-    shallowEqual
+    shallowEqualApp
   )
+
+  // 事件处理函数
+  // 想要调用组件的方法,需要先拿到组件
+  function handlePrevClick() {
+    bannerRef.current?.prev()
+    console.log(bannerRef)
+  }
+  function handleNextClick() {
+    bannerRef.current?.next()
+  }
+
   return (
-    <div>
-      {banners.map((item) => {
-        return <div key={item.imageUrl}>{item.imageUrl}</div>
-      })}
-    </div>
+    <BannerWrapper>
+      <div className="banner wrap-v2">
+        <BannerLeft>
+          <Carousel autoplay ref={bannerRef}>
+            {banners.map((item) => {
+              return (
+                <div className="banner-item" key={item.imageUrl}>
+                  <img
+                    className="image"
+                    src={item.imageUrl}
+                    alt={item.typeTitle}
+                  />
+                </div>
+              )
+            })}
+          </Carousel>
+        </BannerLeft>
+        <BannerRight></BannerRight>
+        <BannerControl>
+          <button className="btn left" onClick={handlePrevClick}></button>
+          <button className="btn right" onClick={handleNextClick}></button>
+        </BannerControl>
+      </div>
+    </BannerWrapper>
   )
 }
 
