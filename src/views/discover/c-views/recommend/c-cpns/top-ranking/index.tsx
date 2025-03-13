08c429d00560076c8bc4f28d2d00ba008af213c1
蜻蜓ink队长 <1512013298@qq.com>
2025-03-13 20:41:24 +0800

    榜单样式

--- src/views/discover/c-views/recommend/c-cpns/top-ranking/index.tsx
+++ src/views/discover/c-views/recommend/c-cpns/top-ranking/index.tsx
@@ -1,18 +1,27 @@
 import { memo } from 'react'
 import type { FC, ReactNode } from 'react'
 
+import { useAppSelector } from '@/store'
 import { RankingWrapper } from './style'
 import AreaHeaderV1 from '@/components/area-header-v1'
+import TopRankingItem from '../top-ranking-item'
 
 interface Iprops {
   children?: ReactNode
 }
 
-const TopRanking: FC<Iprops> = (props) => {
+const TopRanking: FC<Iprops> = () => {
+  const { ranking } = useAppSelector((state) => ({
+    ranking: state.recommend.rankings
+  }))
   return (
     <RankingWrapper>
       <AreaHeaderV1 title="榜单" moreLink="/discover/ranking" />
-      <div className="content"></div>
+      <div className="content">
+        {ranking.map((item) => {
+          return <TopRankingItem key={item.id} itemData={item} />
+        })}
+      </div>
     </RankingWrapper>
   )
 }
