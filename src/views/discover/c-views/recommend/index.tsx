a0b14667960234915aca276c73c2c7eb4a1dd558
蜻蜓ink队长 <1512013298@qq.com>
2025-03-13 19:28:57 +0800

    利用promise.all保证数据的顺序,整体完整

--- src/views/discover/c-views/recommend/index.tsx
+++ src/views/discover/c-views/recommend/index.tsx
@@ -2,7 +2,10 @@ import { memo, useEffect } from 'react'
 import type { FC, ReactNode } from 'react'
 
 import { useAppDispatch } from '@/store'
-import { fetchRecommendDataAction } from './store/recommend'
+import {
+  fetchRecommendDataAction,
+  fetchRankingDataAction
+} from './store/recommend'
 import { RecommendWrapper } from './style'
 import TopBanner from './c-cpns/top-banner'
 import HotRecommend from './c-cpns/hot-recommend'
@@ -19,6 +22,7 @@ const Recommend: FC<IProps> = () => {
     // 直接调用它，虽然可以发起异步请求，
     // 但请求的结果不会被 Redux 捕获，也不会更新全局状态。
     dispatch(fetchRecommendDataAction())
+    dispatch(fetchRankingDataAction())
   }, [])
 
   return (
--- src/views/discover/c-views/recommend/store/recommend.ts
+++ src/views/discover/c-views/recommend/store/recommend.ts
@@ -5,7 +5,14 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 //    它会自动处理异步逻辑，并且与 Redux 的状态管理无缝集成。
 //  当你使用   createAsyncThunk   创建一个异步操作时，
 //    它会返回一个特殊的函数，这个函数可以直接被   dispatch   调用。
-import { getBanners, getHotRecommend, getNewAlbum } from '../service/recommend'
+import {
+  getBanners,
+  getHotRecommend,
+  getNewAlbum,
+  getPlaylistDetail
+} from '../service/recommend'
+import { log } from 'console'
+import { Breadcrumb } from 'antd'
 
 /* 
 export const fetchBannerDataAction = createAsyncThunk(
@@ -49,16 +56,57 @@ export const fetchRecommendDataAction = createAsyncThunk(
   }
 )
 
+// 榜单数据
+const reankgIds = [19723756, 3779629, 2884035]
+export const fetchRankingDataAction = createAsyncThunk(
+  'rankingData',
+  (_, { dispatch }) => {
+    // 1.每个数组单独处理
+    /*    
+    for (const id of reankgIds) {
+      getPlaylistDetail(id).then((res) => {
+        console.log(res)
+        switch (id) {
+          case 19723756:
+            console.log('飙升榜', res)
+            break
+          case 3779629:
+            console.log('新歌榜', res)
+            break
+          case 2884035:
+            console.log('原创榜', res)
+            break
+        }
+      })
+    }
+    */
+    //  2.同一处理
+    // 保障一:获取所有的结果后,进行dispatch操作
+    // 保障二:获取到的结果一定是有正确的顺序
+    let index = 0
+    const promise: Promise<any>[] = []
+    for (const id of reankgIds) {
+      promise.push(getPlaylistDetail(id))
+    }
+    Promise.all(promise).then((res) => {
+      const playlists = res.map((item) => item.playlist)
+      dispatch(changeRankingAction(playlists))
+    })
+  }
+)
+
 interface IRecommendState {
   banners: any[]
   hotRecommends: any[]
   newAlbums: any[]
+  rankings: any[]
 }
 
 const initialState: IRecommendState = {
   banners: [],
   hotRecommends: [],
-  newAlbums: []
+  newAlbums: [],
+  rankings: []
 }
 
 const recommendSlice = createSlice({
@@ -73,6 +121,9 @@ const recommendSlice = createSlice({
     },
     changeNewAlbumAction(state, { payload }) {
       state.newAlbums = payload
+    },
+    changeRankingAction(state, { payload }) {
+      state.rankings = payload
     }
   }
   /*  
@@ -86,7 +137,8 @@ const recommendSlice = createSlice({
 export const {
   changeBannerAction,
   changeHotRecommendAction,
-  changeNewAlbumAction
+  changeNewAlbumAction,
+  changeRankingAction
 } = recommendSlice.actions
 
 export default recommendSlice.reducer
